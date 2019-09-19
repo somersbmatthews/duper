fc9a61e7a36fa67c85d140e2dbe415f67be00653
Somers Matthews <somersbmatthews@gmail.com>
2019-09-19 18:38:14 -0500

    added start() in [worker_supervisor.ex]

--- lib/duper/worker_supervisor.ex
+++ lib/duper/worker_supervisor.ex
@@ -3,6 +3,16 @@ defmodule Duper.WorkerSupervisor do
 
    @me WorkerSupervisor
 
+   def start(_type, _args) do
+      children = [
+         Duper.Results,
+         {Duper.PathFinder, "."},
+         Duper.WorkerSupervisor,
+      ]
+      opts = [strategy: :one_for_one, name: Duper.Supervisor]
+      Supervisor.start_link(children, opts)
+   end
+
    def start_link(_) do
       DynamicSupervisor.start_link(__MODULE__, :no_args, name: @me)
    end
